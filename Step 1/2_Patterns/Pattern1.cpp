651aa4083522a7052ae4375e3de062aaac9ecb2e
m-naman-07 <namancse12411094@iiitsonepat.ac.in>
2025-12-03 10:44:01 +0530

    New folder

---
+++ Step 1/2_Patterns/Pattern1.cpp
@@ -0,0 +1,23 @@
+/*
+Given an integer N, print the following pattern.
+for example N=5
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+*/
+
+#include <iostream>
+using namespace std;
+
+void pattern1(int N){
+
+}
+
+int main(){
+
+
+    return 0;
+}
